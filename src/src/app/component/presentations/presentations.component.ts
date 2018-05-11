cdb15947020c6347b7cd58c1c1b0cb0a71c94152
Pascal ECHEMANN <pechemann@gmail.com>
2018-05-11 11:41:13 +0200

    Documentation Update
    Removing deprecated content.

--- src/src/app/component/presentations/presentations.component.ts
+++ src/src/app/component/presentations/presentations.component.ts
@@ -30,8 +30,7 @@ export class PresentationsComponent extends AbstractViewComponent implements Aft
       { title: "JCAD", img: "jec-jcad-thumb", pdf: "jec-jcad" },
       { title: "JARS Overview", img: "jec-jars-overview-thumb", pdf: "jec-jars-overview" },
       { title: "JUTA Overview", img: "jec-juta-overview-thumb", pdf: "jec-juta-overview" },
-      { title: "App Sample", img: "jec-modular-app-sample-thumb", pdf: "jec-modular-app-sample" },
-      { title: "Understanding Angular GPM", img: "jec-angular-gpm-thumb", pdf: "jec-understanding-angular-gpm" }
+      { title: "App Sample", img: "jec-modular-app-sample-thumb", pdf: "jec-modular-app-sample" }
     ];
   }
 
